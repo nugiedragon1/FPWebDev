e20cd079aec1aa87bb9f09af6410cedbfb9ed9e5
fernndzaky <43334250+fernndzaky@users.noreply.github.com>
2020-04-16 23:46:41 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    THIS IS DZAKY
+                    THIS IS KENNY
                 </div>
 
                 <div class="links">
